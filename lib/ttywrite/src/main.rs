e1fde6ee3af98525ed5aa9a03d272f77370f88fc
bboggess <b.boggess727@gmail.com>
2023-02-11 14:53:36 -0600

    Created ttywrite utility for writing to pi

--- lib/ttywrite/src/main.rs
+++ lib/ttywrite/src/main.rs
@@ -1,49 +1,91 @@
 mod parsers;
 
-use serial;
+use serial::{
+    self,
+    unix::{TTYPort, TTYSettings},
+};
 use structopt;
 use structopt_derive::StructOpt;
-use xmodem::Xmodem;
+use xmodem::{Progress, Xmodem};
 
 use std::path::PathBuf;
 use std::time::Duration;
 
+use serial::core::{BaudRate, CharSize, FlowControl, SerialDevice, SerialPortSettings, StopBits};
 use structopt::StructOpt;
-use serial::core::{CharSize, BaudRate, StopBits, FlowControl, SerialDevice, SerialPortSettings};
 
-use parsers::{parse_width, parse_stop_bits, parse_flow_control, parse_baud_rate};
+use parsers::{parse_baud_rate, parse_flow_control, parse_stop_bits, parse_width};
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "Write to TTY using the XMODEM protocol by default.")]
 struct Opt {
-    #[structopt(short = "i", help = "Input file (defaults to stdin if not set)", parse(from_os_str))]
+    #[structopt(
+        short = "i",
+        help = "Input file (defaults to stdin if not set)",
+        parse(from_os_str)
+    )]
     input: Option<PathBuf>,
 
-    #[structopt(short = "b", long = "baud", parse(try_from_str = "parse_baud_rate"),
-                help = "Set baud rate", default_value = "115200")]
+    #[structopt(
+        short = "b",
+        long = "baud",
+        parse(try_from_str = "parse_baud_rate"),
+        help = "Set baud rate",
+        default_value = "115200"
+    )]
     baud_rate: BaudRate,
 
-    #[structopt(short = "t", long = "timeout", parse(try_from_str),
-                help = "Set timeout in seconds", default_value = "10")]
+    #[structopt(
+        short = "t",
+        long = "timeout",
+        parse(try_from_str),
+        help = "Set timeout in seconds",
+        default_value = "10"
+    )]
     timeout: u64,
 
-    #[structopt(short = "w", long = "width", parse(try_from_str = "parse_width"),
-                help = "Set data character width in bits", default_value = "8")]
+    #[structopt(
+        short = "w",
+        long = "width",
+        parse(try_from_str = "parse_width"),
+        help = "Set data character width in bits",
+        default_value = "8"
+    )]
     char_width: CharSize,
 
     #[structopt(help = "Path to TTY device", parse(from_os_str))]
     tty_path: PathBuf,
 
-    #[structopt(short = "f", long = "flow-control", parse(try_from_str = "parse_flow_control"),
-                help = "Enable flow control ('hardware' or 'software')", default_value = "none")]
+    #[structopt(
+        short = "f",
+        long = "flow-control",
+        parse(try_from_str = "parse_flow_control"),
+        help = "Enable flow control ('hardware' or 'software')",
+        default_value = "none"
+    )]
     flow_control: FlowControl,
 
-    #[structopt(short = "s", long = "stop-bits", parse(try_from_str = "parse_stop_bits"),
-                help = "Set number of stop bits", default_value = "1")]
+    #[structopt(
+        short = "s",
+        long = "stop-bits",
+        parse(try_from_str = "parse_stop_bits"),
+        help = "Set number of stop bits",
+        default_value = "1"
+    )]
     stop_bits: StopBits,
 
     #[structopt(short = "r", long = "raw", help = "Disable XMODEM")]
     raw: bool,
+}
+
+fn progress_tracker(progress: Progress) {
+    match progress {
+        Progress::Waiting => println!("Establishing connection..."),
+        Progress::Started => println!("Connection established!"),
+        Progress::Packet(num) => println!("Sent packet number: {}", num),
+        Progress::NAK => (),
+        Progress::Unknown => (),
+    }
 }
 
 fn main() {
@@ -53,5 +95,28 @@ fn main() {
     let opt = Opt::from_args();
     let mut port = serial::open(&opt.tty_path).expect("path points to invalid TTY");
 
-    // FIXME: Implement the `ttywrite` utility.
+    let mut settings = port.read_settings().unwrap();
+    settings
+        .set_baud_rate(opt.baud_rate)
+        .expect("invalid baud rate");
+    settings.set_char_size(opt.char_width);
+    settings.set_flow_control(opt.flow_control);
+    settings.set_stop_bits(opt.stop_bits);
+    port.write_settings(&settings).unwrap();
+    port.set_timeout(Duration::from_secs(opt.timeout)).unwrap();
+
+    let mut input: Box<dyn io::BufRead> = match opt.input {
+        Some(path) => Box::new(BufReader::new(
+            File::open(path).expect("invalid input file path"),
+        )),
+        None => Box::new(BufReader::new(io::stdin())),
+    };
+
+    let bytes_written = if opt.raw {
+        io::copy(&mut input, &mut port).unwrap()
+    } else {
+        Xmodem::transmit_with_progress(input, port, progress_tracker).unwrap() as u64
+    };
+
+    println!("Wrote {} bytes", bytes_written);
 }
