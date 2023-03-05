bdc140b5e28865d95b46b9cffba489ba5fbb61f8
bboggess <b.boggess727@gmail.com>
2023-03-05 16:12:11 -0600

    Add a newline so panic message is easier to read

--- kern/src/init/panic.rs
+++ kern/src/init/panic.rs
@@ -3,6 +3,7 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+    kprintln!("");
     kprintln!("         ¯\\_(ツ)_/¯");
     kprintln!("---------- PANIC ----------");
     kprintln!("");
